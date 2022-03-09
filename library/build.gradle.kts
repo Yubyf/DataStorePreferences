b1fa4fb57a62910072586d9ad70346c2f9114e5c
Yubyf <lpy19920505@gmail.com>
2022-03-09 22:36:10 +0800

    Added publishing script for mavenCentral.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
 }
 android {
     compileSdk = 31
@@ -39,3 +43,86 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
+
+//region Publish
+val snapshot: String by project
+val buildType = "release"
+val mavenGroupId: String by project
+val mavenArtifactId: String by project
+val mavenVersion: String by project
+val pomName: String by project
+val pomDescription: String by project
+val pomUrl: String by project
+val pomLicenseName: String by project
+val pomLicenseUrl: String by project
+val developerName: String by project
+val developerEmail: String by project
+
+// Maven server credentials in local.properties
+val localProperties = gradleLocalProperties(rootDir)
+
+val sourcesJar by tasks.register("sourcesJar", Jar::class) {
+    from(android.sourceSets["main"].java.srcDirs)
+    archiveClassifier.set("sources")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri("https://s01.oss.sonatype.org/"
+                    + if (snapshot.toBoolean()) "content/repositories/snapshots/" else "service/local/staging/deploy/maven2/")
+            credentials {
+                username = localProperties["OSSRH_USERNAME"] as String?
+                password = localProperties["OSSRH_PASSWORD"] as String?
+            }
+        }
+    }
+
+    publications {
+        afterEvaluate {
+            create<MavenPublication>(buildType) {
+                from(components[buildType])
+
+                artifact(sourcesJar)
+
+                groupId = mavenGroupId
+                artifactId = mavenArtifactId
+                version = "$mavenVersion${if (snapshot.toBoolean()) "-SNAPSHOT" else ""}"
+
+                pom {
+                    packaging = "aar"
+                    name.set(mavenGroupId)
+                    description.set(pomDescription)
+                    url.set(pomUrl)
+                    licenses {
+                        license {
+                            name.set(pomLicenseName)
+                            url.set(pomLicenseUrl)
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set(developerName)
+                            email.set(developerEmail)
+                        }
+                    }
+                    scm {
+                        url.set(pom.url.get())
+                        connection.set("scm:git:${url.get()}.git")
+                        developerConnection.set("scm:git:${url.get()}.git")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    // Load gpg info in gradle.properties(global)
+    useGpgCmd()
+    afterEvaluate {
+        sign(publishing.publications[buildType])
+    }
+}
+//endregion
